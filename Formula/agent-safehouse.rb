3c302d0c84fc45e60fa1ac46b0835c66189be338
Eugene G <eugene7@gmail.com>
2026-03-11 16:03:11 +0100

    agent-safehouse v0.1.0

--- Formula/agent-safehouse.rb
+++ Formula/agent-safehouse.rb
@@ -1,15 +1,15 @@
 class AgentSafehouse < Formula
   desc "macOS sandbox wrapper for coding agents"
   homepage "https://github.com/eugene1g/agent-safehouse"
+  url "https://github.com/eugene1g/agent-safehouse/releases/download/v0.1.0/safehouse.sh"
+  version "0.1.0"
+  sha256 "9ed69b10aeb9d0c08fc1562d0e1ff758e0ddd858246d828412dc556c79733714"
   license "Apache-2.0"
   head "https://github.com/eugene1g/agent-safehouse.git", branch: "main"
 
   def install
     odie "Agent Safehouse requires macOS" unless OS.mac?
-
-    out = buildpath/"brew-dist"
-    system "./scripts/generate-dist.sh", "--output", out/"safehouse", "--output-dir", out
-    bin.install out/"safehouse"
+    bin.install "safehouse.sh" => "safehouse"
   end
 
   test do
