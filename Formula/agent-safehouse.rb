2971ab153a7e04e6b224daea109e940d92897896
Eugene G <eugene7@gmail.com>
2026-03-17 21:32:29 +0100

    agent-safehouse v0.5.0

--- Formula/agent-safehouse.rb
+++ Formula/agent-safehouse.rb
@@ -1,9 +1,9 @@
 class AgentSafehouse < Formula
   desc "macOS sandbox wrapper for coding agents"
   homepage "https://github.com/eugene1g/agent-safehouse"
-  url "https://github.com/eugene1g/agent-safehouse/releases/download/v0.4.0/safehouse.sh"
-  version "0.4.0"
-  sha256 "9866421da283351131d4bb558e6ffc68c857eab74eb8ac94c77fb3ec70dcb188"
+  url "https://github.com/eugene1g/agent-safehouse/releases/download/v0.5.0/safehouse.sh"
+  version "0.5.0"
+  sha256 "dee798d23f9cf4e89d2c6a55c246c1713ddcd1fb5a3168c2f79141ae5875767f"
   license "Apache-2.0"
   head "https://github.com/eugene1g/agent-safehouse.git", branch: "main"
 
